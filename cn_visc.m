f15a4c687726793d4f227031b637224cf0a2e5a6
francesco-lomonaco <f.lomonaco9@gmail.com>
2016-02-12 15:04:16 +0100

    fix incontro

--- cn_visc.m
+++ cn_visc.m
@@ -1,24 +1,29 @@
 %r = dt/dx*dx
 function unew = cn_visc( u, dt, dx, visc )
-r = -visc*(dt/dx*dx);
+r = -visc*(dt/(dx*dx));
 N = length( u );
-w = zeros( N, 1 );
-A = eye( N )*(1-r);
+w = zeros( N-1, 1 );
+A = eye( N-1 )*(1-r);
 
-for ii = 1:(N-1)
+for ii = 1:(N-2)
     A(ii, ii+1) = r*0.5;
 end
-for ii = 2:N
+for ii = 2:N-1
     A(ii, ii-1) = r*0.5;
 end
-u(1)=0;
-u(N)=0;
-u(N/2)=0;
+
+    w(1) = u(1)-(dt/4*dx)*((u(2).^2)-(u(N-1).^2)) + ...
+    ((dt.^2)/(8*(dx.^2)))*((u(1)+u(2))*((u(2).^2)-(u(1).^2))-(u(1)+u(N-1))*((u(1).^2)-(u(N-1).^2))) + ...
+    ((dt*visc)/(2*(dx.^2)))*(u(2)-2*u(1)+u(N-1)); %- 2*r * u(ii);
+
 for ii = 2:N-1
     %w(ii) = u(ii) - 2*r * u(ii);
 %    w(ii) = u(ii) - 0.25*r * (u(ii+1) - u(ii-1));
-    w(ii) = u(ii)-(dt/4*dx)*((u(ii+1).^2)-(u(ii-1).^2)) + ...
+    w(ii) = u(ii)-(dt/(4*dx))*((u(ii+1).^2)-(u(ii-1).^2)) + ...
     ((dt.^2)/(8*(dx.^2)))*((u(ii)+u(ii+1))*((u(ii+1).^2)-(u(ii).^2))-(u(ii)+u(ii-1))*((u(ii).^2)-(u(ii-1).^2))) + ...
     ((dt*visc)/(2*(dx.^2)))*(u(ii+1)-2*u(ii)+u(ii-1)); %- 2*r * u(ii);
 end
-unew = A \ w;
+    
+unew(1:N-1) = A \ w;
+
+unew(N)=unew(1);
